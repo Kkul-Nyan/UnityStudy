0512e4ab2103a8a947546d371a9edae23d2ee572
Hyeonki <sadpo5660@gmail.com>
2023-08-11 21:33:21 +0900

    아이탬데이터컨테이너

---
+++ 3DHDRP/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+     PlayerStatus playerStatus;
+
+     public float decoyStamina;
+
+     public float playerSpeed;
+     public float jumpPower;
+     Rigidbody rig;
+     Vector2 inputVec;
+
+     public Transform player;
+     public Transform cameraContainer;
+     public float minXLook;
+     public float maxXLook;
+     float camCurXRot;
+     public float lookSensitivity;
+
+     bool canLook = true;
+     Vector2 mouseDelta;
+
+     Animator anim;
+
+     bool isMovementAllowed = true;
+     float immobilizeEndTime = 0f;
+     public float immobilizeDuration = 3f;
+
+     private void Awake(){
+          rig = GetComponent<Rigidbody>();
+          anim = GetComponentInChildren<Animator>();
+          playerStatus = GetComponent<PlayerStatus>();
+     }
+
+     private void Start(){
+          Cursor.lockState = CursorLockMode.Locked;
+     }
+
+      private void FixedUpdate()
+     {
+          if (isMovementAllowed)
+          {
+               PlayerMove();
+          }
+          else
+          {
+               if (Time.time >= immobilizeEndTime)
+               {
+                    anim.SetBool("Tired", false);
+                    isMovementAllowed = true;
+               }
+          }
+     }
+
+
+     private void LateUpdate(){
+          if(canLook == true){
+               CameraLook();
+          }
+     }
+
+
+     public void OnMoveInput(InputAction.CallbackContext callback){
+          if(isMovementAllowed){
+               if(callback.phase == InputActionPhase.Performed){  
+                    anim.SetBool("Run", true);
+                    inputVec = callback.ReadValue<Vector2>();
+               }
+               else{
+                    anim.SetBool("Run", false);
+                    inputVec = Vector2.zero;
+               }
+          }
+          
+     }
+
+     void PlayerMove()
+     {
+          if (inputVec != Vector2.zero)
+          {
+               playerStatus.Work(decoyStamina * Time.deltaTime);
+               if (playerStatus.stamina.curValue <= 0f)
+               {
+                    isMovementAllowed = false;
+                    immobilizeEndTime = Time.time + immobilizeDuration;
+                    anim.SetBool("Tired", true);
+                    anim.SetBool("Run", false);
+                    inputVec = Vector2.zero;
+                    return; // 스태미너가 부족하면 이동하지 않습니다.
+               }
+          }
+
+          Vector3 moveVec = transform.forward * inputVec.y + transform.right * inputVec.x;
+          moveVec *= playerSpeed;
+          moveVec.y = rig.velocity.y;
+          rig.velocity = moveVec;
+     }
+
+   
+
+     public void OnJumpInput(InputAction.CallbackContext callback){
+          if(callback.phase == InputActionPhase.Performed){
+               rig.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+          }
+     }
+
+     public void OnLookInput(InputAction.CallbackContext context){
+          mouseDelta = context.ReadValue<Vector2>();
+     }
+
+     void CameraLook(){
+     camCurXRot += mouseDelta.y * lookSensitivity;
+     camCurXRot = Mathf.Clamp(camCurXRot, minXLook, maxXLook);
+     cameraContainer.localEulerAngles = new Vector3 (-camCurXRot, 0, 0);
+
+     transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
+     }
+
+     public void ToggleCursor(bool toggle){
+          Cursor.lockState = toggle ? CursorLockMode.None : CursorLockMode.Locked;
+          canLook = !toggle;
+     }
+}
---
+++ 3DHDRP/Assets/Scripts/PlayerStatus.cs
@@ -0,0 +1,152 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStatus : MonoBehaviour, IDamagable
+{
+    public Status health;
+    public Status stamina;
+    public Status mana;
+    public Status hunger;
+    public Status thirst;
+    public Status sleep;
+
+    public float noHungerHealthDecay;
+    public float noThirstHealthDecay;
+
+    public bool isBattle = false;
+    public float notBattleTime = 60;
+
+    public bool isHunger = false;
+    public float keepStaminaMax;
+
+    void Start(){
+        health.curValue = health.startValue;
+        stamina.curValue = stamina.startValue;
+        mana.curValue = mana.startValue;
+        hunger.curValue = hunger.startValue;
+        thirst.curValue = thirst.startValue;
+        sleep.curValue = sleep.startValue;
+        keepStaminaMax = stamina.maxValue;
+    }
+
+    void Update(){
+        StatusUpdate();
+        HungrySound();
+    }
+
+    void StatusUpdate(){
+        CheckPlayerDead();
+
+        stamina.maxValue = Mathf.Max(hunger.curValue, keepStaminaMax * 0.3f);
+
+        if(isBattle == false){
+            stamina.Add(stamina.regenRate * Time.deltaTime);
+            mana.Add(thirst.regenRate * Time.deltaTime);
+        }
+        
+        hunger.Subtract(hunger.decayRate * Time.deltaTime);
+        thirst.Subtract(thirst.decayRate * Time.deltaTime);
+        sleep.Add(sleep.regenRate * Time.deltaTime);
+
+        if(hunger.curValue == 0.0f){
+            health.Subtract(noHungerHealthDecay * Time.deltaTime);
+        }
+        if(thirst.curValue == 0.0f){
+            health.Subtract(noThirstHealthDecay * Time.deltaTime);
+        }
+        
+        health.uiBar.fillAmount = health.GetPercentage();
+        stamina.uiBar.fillAmount = stamina.GetPercentage();
+        mana.uiBar.fillAmount = mana.GetPercentage();
+        hunger.uiBar.fillAmount = hunger.GetPercentage();
+        thirst.uiBar.fillAmount = thirst.GetPercentage();
+        //sleep.uiBar.fillAmount = sleep.GetPercentage();
+    }
+
+    void HungrySound(){
+        if( hunger.curValue < 40){
+            if(!isHunger){
+                //오디오 실행
+                isHunger = true;
+            }
+        }
+        else{
+            isHunger = false;
+        }
+        
+
+    }
+    void CheckPlayerDead(){
+        if(health.curValue == 0.0f){
+            Die();
+        }
+    }
+
+    public void Heal(float amount){
+        health.Add(amount);
+    }
+    public void Work(float amount){
+        stamina.Subtract(amount);
+    }
+    public void Magic(float amount){
+        mana.Subtract(amount);
+    }
+
+    public void rest(float amount){
+        stamina.Add(amount);
+    }
+
+    public void restMana(float amount){
+        mana.Add(amount);
+    }
+    public void Eat(float amount){
+        hunger.Add(amount);
+    }
+    public void Drink(float amount){
+        thirst.Add(amount);
+    }
+    public void Sleep(float amount){
+        sleep.Subtract(amount);
+    }
+    public void TakePhysicalDamage(int amount){
+        health.Subtract(amount);
+        notBattleTime = 60;
+        isBattle = true;
+    }
+
+    public void CheckBattle(){
+        if(notBattleTime > 0){
+            notBattleTime -= Time.deltaTime;
+        }
+        else{
+            isBattle = false;
+        }
+    }
+
+    public void Die(){
+        Debug.Log("Player is Dead");
+    }
+}
+
+[System.Serializable]
+public class Status
+{
+    public float maxValue;
+    public float curValue;
+    public float startValue;
+    public float regenRate;
+    public float decayRate;
+    public Image uiBar;
+
+    public void Add(float amount){
+        curValue = Mathf.Min(curValue + amount, maxValue);
+    }
+    public void Subtract (float amount) {
+        curValue = Mathf.Max(curValue - amount, 0.0f);
+    }
+    public float GetPercentage (){
+        return curValue / maxValue;
+    }
+}

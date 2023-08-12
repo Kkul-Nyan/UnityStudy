968864f9468580fedc9f506d874679e4cdf177b6
Hyeonki <sadpo5660@gmail.com>
2023-08-12 18:46:35 +0900

    아이탬데이터컨테이너
    아이탬데이터컨테이너

---
+++ 3DHDRP/Assets/Scripts/Item/ItemData.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+[EnumToggleButtons]
+public enum ItemType{
+    Resource,
+    Equipable,
+    Consumable
+}
+
+[EnumToggleButtons]
+public enum ConsumableType {
+    Health,
+    Stamina,
+    Mana,
+    Hunger,
+    Thirst,
+    Sleep
+}
+
+[CreateAssetMenu(fileName = "Item", menuName = "New Item Data")]
+public class ItemData : ScriptableObject
+{
+    public string id;
+    [BoxGroup("Info")]public string displayName;
+    [BoxGroup("Info")]public string description;
+    [BoxGroup("Info")]public ItemType type;
+
+    [BoxGroup("Info")][PreviewField(100, ObjectFieldAlignment.Center)]public Sprite icon;
+    [BoxGroup("Info")]public GameObject dropPrefab;
+
+    
+    [BoxGroup("Stacking")]public bool canStack;
+    [BoxGroup("Stacking")]public int maxStackAmount;
+
+    [BoxGroup("Consumable")]
+    public ItemDataConsumable[] consumable;
+
+    [BoxGroup("Equip")]
+    public GameObject equipPrefab;
+}
+
+[System.Serializable]
+public class ItemDataConsumable
+{
+    public ConsumableType type;
+    public float value;
+}

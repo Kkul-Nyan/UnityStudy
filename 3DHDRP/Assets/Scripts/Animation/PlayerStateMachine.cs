ffdfd91e7c24ba3a131d6c243082f9628bae6745
Hyeonki <sadpo5660@gmail.com>
2023-10-04 23:41:57 +0900

    inputReader 스크립트와 State스크립트 연동
    점프메서드 inputReader에 구독

--- 3DHDRP/Assets/Scripts/Animation/PlayerStateMachine.cs
+++ 3DHDRP/Assets/Scripts/Animation/PlayerStateMachine.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class PlayerStateMachine : StateMachine
 {
+    [field : SerializeField] public InputReader InputReader { get; private set;}
+
+
     private void Start() {
         SwitchState(new PlayerTestState(this));
     }
--- 3DHDRP/Assets/Scripts/Animation/PlayerTestState.cs
+++ 3DHDRP/Assets/Scripts/Animation/PlayerTestState.cs
@@ -1,31 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerTestState : PlayerBaseState
 {
-    float timer = 5f;
+    float timer;
 
     public PlayerTestState(PlayerStateMachine stateMachine) : base(stateMachine) {}
 
     public override void Enter()
     {
-        Debug.Log("Enter");
+        stateMachine.InputReader.JumpEvent += OnJump;
     }
     public override void Tick(float deltaTime)
     {
-        timer -= deltaTime;
+        timer += deltaTime;
         Debug.Log(timer);
 
-        if(timer <= 0f){
-            stateMachine.SwitchState(new PlayerTestState(stateMachine));
-        }
-        
     }
     public override void Exit()
     {
-        Debug.Log("Exit");
-        
+        stateMachine.InputReader.JumpEvent -= OnJump;
+    }
+
+    void OnJump(){
+        stateMachine.SwitchState(new PlayerTestState(stateMachine));
     }
 
 }

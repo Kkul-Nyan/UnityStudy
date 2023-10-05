7a4b28013e258da9b5864d2a0e9ab5b47e24a8f9
Hyeonki <sadpo5660@gmail.com>
2023-10-05 23:43:40 +0900

    statemachine 기반 inputAction처리

--- 3DHDRP/Assets/Scripts/Animation/PlayerTestState.cs
+++ 3DHDRP/Assets/Scripts/Animation/PlayerTestState.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEditor;
 using UnityEngine;
 
@@ -16,10 +17,15 @@ public class PlayerTestState : PlayerBaseState
     public override void Tick(float deltaTime)
     {
         Vector3 movement = new Vector3();
+
         movement.x = stateMachine.InputReader.MovementValue.x;
         movement.y = 0;
         movement.z = stateMachine.InputReader.MovementValue.y;
-        stateMachine.transform.Translate(movement * deltaTime);
+
+        stateMachine.CharacterController.Move(movement * stateMachine.FreeLookMovementSpeed * deltaTime);
+
+        if(stateMachine.InputReader.MovementValue == Vector2.zero) { return; }
+        stateMachine.transform.rotation = Quaternion.LookRotation(movement);
     }
     public override void Exit()
     {
